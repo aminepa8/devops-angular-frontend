416d693a5f873b2303fa742847060d42b0a23f1a
Amine Moulay <aminepa8@gmail.com>
2021-03-01 11:52:02 +0100

    Updated the baseUrl in protractor config file

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -17,7 +17,7 @@ exports.config = {
   },
   directConnect: true,
   SELENIUM_PROMISE_MANAGER: false,
-  baseUrl: 'http://localhost:4200/',
+  baseUrl: 'http://40.121.167.90',
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
